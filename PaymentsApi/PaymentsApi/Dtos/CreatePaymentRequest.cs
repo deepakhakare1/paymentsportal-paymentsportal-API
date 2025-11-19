bd6b70a79e3b32b1080dd17450c3a4a76e712a85
Deepak Hakare <deepak.hakare13@gmail.com>
2025-11-19 11:42:51 +0530

    added regularExpression validation for DTO CreatePaymentRequest class

--- PaymentsApi/PaymentsApi/Dtos/CreatePaymentRequest.cs
+++ PaymentsApi/PaymentsApi/Dtos/CreatePaymentRequest.cs
@@ -4,13 +4,16 @@ namespace PaymentsApi.Dtos
     public class CreatePaymentRequest
     {
         [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
         public decimal Amount { get; set; }
 
         [Required]
+        [RegularExpression("^(USD|EUR|INR|GBP)$", ErrorMessage = "Currency must be USD, EUR, INR, or GBP")]
         public string Currency { get; set; } = null!;
 
         // clientRequestId must be provided by client (GUID recommended)
         [Required]
+        [MaxLength(100)]
         public string ClientRequestId { get; set; } = null!;
     }
 }
